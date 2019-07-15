1db85b570632a3b62efdd5cc4b73956482c9fbc7
yforyuri <yforyurirho@gmail.com>
2019-07-15 09:44:10 +0900

    JSP server update

---
+++ JAVA/Firstjava/src/first/First.java
@@ -0,0 +1,11 @@
+package first;
+
+public class First {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, world!");
+		System.out.println("it's very first time!");
+	}
+
+}
