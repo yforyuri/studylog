888985f1de0bc6d65a54f71ae152847b461400a2
yforyuri <yforyurirho@gmail.com>
2019-07-31 17:56:01 +0900

    spring mvc
    classwork

---
+++ SPRING/Spring-MVC/src/main/java/com/bitcamp/mvc/HelloController.java
@@ -0,0 +1,27 @@
+package com.bitcamp.mvc;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+
+	@RequestMapping("/hello")
+	public ModelAndView hello() {
+		
+		// FrontController로 전송할 view경로와 결과데이터 저장하고 전달할 객체 
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("hello"); //WEB-INF/views/hello.jsp
+		modelAndView.addObject("userName", "Y");
+		modelAndView.addObject("greeting", "hi");
+		modelAndView.addObject("now", new Date());
+		
+		return modelAndView;
+	}
+	
+	
+}
---
+++ SPRING/Spring-MVC/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>Hello.jsp</h1>
+	<h3>
+		${userName}<br>
+		${greeting}<br>
+		${now}<br>
+	</h3>
+</body>
+</html>
